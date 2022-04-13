25d358fade9c877f29acb50bf6b0895c5540195b
user <user@DESKTOP-7538O1O>
2022-04-13 16:09:36 +0900

    2022-04-13

---
+++ src/com/javaex/ex01/Ex01.java
@@ -0,0 +1,61 @@
+package com.javaex.ex01;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		int myAge;  //변수선언과 초기화를 동시에
+		myAge = 25;
+		
+		System.out.println(myAge);
+		 
+		myAge = 26; System.out.println(myAge);
+		*/
+		
+		
+		
+		/*
+		byte no;
+		no = -128;
+		System.out.println(no);
+		
+		// int 범위까지는 L생략가능, long 범위부터는 L을 붙여야한다
+		long no2;
+		no2 = 2147483648L;
+		System.out.println(no2);
+		*/
+		
+		
+		//////////////////////////
+		//변수선언, 초기화
+		//////////////////////////
+		
+		//변수가 여러개일 때 선언 후 초기화
+		/*
+		int var01;
+		int var02;
+		int var03;
+		*/
+		int var01, var02, var03;
+		
+		var01 = 10;
+		var02 = 20;
+		var03 = 30;
+		
+		//변수가 여러개일 때 선언+초기화
+		
+		int var04 = 10;
+		int var05 = 20;
+		int var06 = 30;
+		
+		//int var04=10, var05=20, var06=30;
+		
+		
+		
+		
+		
+		
+	}
+
+}
